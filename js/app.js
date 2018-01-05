8e57bfbba34219ac1d2fd746ff30032a67142d56
Pattor <pa77or@gmail.com>
2018-01-04 22:01:54 -0500

    Adding metainfo

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
   $("#nav li").mouseenter(function(e){
     if("nav__menu__submenu__item" === e.currentTarget.classList[1]){
       var w = $(this).children().eq(0).width();
-      console.log(w);
+
       $(this).children().eq(1).css('left',w+5);
     }
   });
@@ -34,12 +34,23 @@ $(document).ready(function() {
     let children = grid.children();
     for (let i = 0; i < children.length; i++) {
       ///SETTING ALL THE HEIGHTS OF THE CARDS
-      BuildingCard(children.eq(i));
+      if(i==0){//if is the header
+        BuildingCard(children.eq(i),i);
+      }
+      else{
+        BuildingCard(children.eq(i));//get just the article
+      }
     }
   }
 
-  function BuildingCard(card){
-    let array = card.children();
+  function BuildingCard(card,o=-1){
+    let array;
+    if(o==0){
+      array = card.children();
+    }
+    else{
+      array = card.children().eq(1);//just the children of article
+    }
     let height = 0;
     for (var i = 0; i < array.length; i++) {
       if(array.eq(i).css("display") === "none" || array.eq(i).hasClass("not-count")){
@@ -48,6 +59,8 @@ $(document).ready(function() {
       }
       else if(array.eq(i).hasClass("card__img-to-the-left-layout") || array.eq(i).hasClass("card__img-to-the-right-layout")){
         height = FloatingLayouts(card);
+        console.log("Inside floating layout");
+        console.log(height);
         break;
       }
       else{
@@ -70,8 +83,6 @@ $(document).ready(function() {
     let floatImgHeight = 0;
     let contentHeight = 0;
     for (var i = 0; i < array.length; i++) {
-
-      console.log(array.eq(i));
       if(array.eq(i).css("display") === "none" || array.eq(i).hasClass("not-count")){
         //if the child/div is a "card__img-to-the-left-layout" DONT COUNT IT
         continue;
